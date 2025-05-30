137ff9e56e302d2dc6dc966c0bcf21fc3441f0d5
Gustavo <gugaliisboa2@gmail.com>
2025-05-30 03:38:26 -0300

    Commit 3.6-1 - Melhorias no Dashboard que já está funcional

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,15 @@ class DashboardController < ApplicationController
     @total_classrooms = Classroom.count
     @total_sessions = Session.count
 
-    # Exemplo de @sexo_distribution
-    @sexo_distribution = { "Masculino" => 120, "Feminino" => 100 }
+    # Distribuição de sexo dos alunos
+    @sexo_distribution = Aluno.group(:sexo).count
+
+    # Calculando a idade dos alunos, ignorando alunos sem data de nascimento
+    @idade_distribution = Aluno.all.group_by do |aluno|
+      next if aluno.data_nascimento.nil?
+      Date.today.year - aluno.data_nascimento.year
+    end.compact
+
+    @idade_distribution = @idade_distribution.transform_values(&:count)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Adicionando o Chartkick, Chart.js do CDN
+pin "chartkick", to: "https://cdn.skypack.dev/chartkick"
+pin "chart.js", to: "https://cdn.skypack.dev/chart.js"
+pin "chartjs-adapter-date-fns", to: "https://cdn.skypack.dev/chartjs-adapter-date-fns"
