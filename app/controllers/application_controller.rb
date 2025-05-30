2b9c1fddb15b79d8afa0513af1c83a31c42ab638
Gustavo <gugaliisboa2@gmail.com>
2025-05-30 20:19:38 -0300

    3.6-7 - Correção na importação do Flowbite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :require_authentication
-  before_action :set_current_user  # Adiciona a definição do @current_user
+  before_action :set_current_user
+
+  helper_method :current_user
+
+  private
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id]) if session[:user_id]  # Busca o usuário pela sessão
+  end
+
+  def current_user
+    @current_user
   end
 end
